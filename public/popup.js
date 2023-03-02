d268cb4f2723dcbe77bfa85230ccde4e7f5fb06b
Samuel Hinchliffe <88140540+Samuel-Hinchliffe@users.noreply.github.com>
2023-03-02 14:11:24 +0000

    False disable fix
    Fixed a bug that would set the UI into a locked state.

--- public/popup.js
+++ public/popup.js
@@ -1,1 +1,61 @@
-const toggle=document.getElementById("toggle"),statusText=document.getElementById("status"),robot=document.getElementById("robot");function setRobotStatus(e,t,s=!0){statusText.innerHTML=e,robot.src=t,toggle.checked=s}function messageActiveTab(e,t=(()=>{})){chrome.tabs.query({active:!0,currentWindow:!0},(function(s){chrome.tabs.sendMessage(s[0].id,e,t)}))}toggle.checked=!0,statusText.innerHTML="Idle",messageActiveTab({type:"isBoardPresent"},(e=>{e?chrome.storage.local.get(["isSolving"],(({isSolving:e})=>{toggle.checked?setRobotStatus(e?"Solving":"Idle",e?"solving.svg":"idle.svg"):setRobotStatus("Disabled","sleeping.svg")})):setRobotStatus("No Board","sleeping.svg",!1)})),chrome.storage.onChanged.addListener((function(e){"isSolving"in e&&toggle.checked&&messageActiveTab({type:"isBoardPresent"},(function(t){t&&(e.isSolving.newValue?(statusText.innerHTML="Solving",robot.src="solving.svg"):(statusText.innerHTML="Idle",robot.src="idle.svg"))})),"enabled"in e&&(toggle.checked=e.enabled.newValue,statusText.innerHTML=e.enabled.newValue?"Solving":"Disabled",robot.src=e.enabled.newValue?"solving.svg":"sleeping.svg")})),toggle.addEventListener("change",(function(){if(this.checked)return chrome.storage.local.set({enabled:!0}),void setRobotStatus("Solving","solving.svg");chrome.storage.local.set({enabled:!1}),setRobotStatus("Idle","idle.svg")}));
+const toggle = document.getElementById("toggle");
+const statusText = document.getElementById("status");
+const robot = document.getElementById("robot");
+
+toggle.checked = true;
+statusText.innerHTML = "Idle";
+
+messageActiveTab({ type: "isBoardPresent" }, (response) => {
+	if (!response){
+		setRobotStatus("No Board", "sleeping.svg");
+		toggle.checked = false;
+	}
+	else
+		chrome.storage.local.get(["isSolving"], ({ isSolving }) => {
+			if (!toggle.checked) setRobotStatus("Disabled", "sleeping.svg");
+			else setRobotStatus(isSolving ? "Solving" : "Idle", isSolving ? "solving.svg" : "idle.svg");
+		});
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+	if ("isSolving" in changes && toggle.checked) {
+		messageActiveTab({ type: "isBoardPresent" }, function (response) {
+			if (response) {
+				if (changes.isSolving.newValue) {
+					statusText.innerHTML = "Solving";
+					robot.src = "solving.svg";
+				} else {
+					statusText.innerHTML = "Idle";
+					robot.src = "idle.svg";
+				}
+			}
+		});
+	}
+	if ("enabled" in changes) {
+		toggle.checked = changes.enabled.newValue;
+		statusText.innerHTML = changes.enabled.newValue ? "Solving" : "Disabled";
+		robot.src = changes.enabled.newValue ? "solving.svg" : "sleeping.svg";
+	}
+});
+
+toggle.addEventListener("change", function () {
+	if (toggle.checked) {
+		chrome.storage.local.set({ enabled: true });
+		setRobotStatus("Solving", "solving.svg");
+		return;
+	} else {
+		chrome.storage.local.set({ enabled: false });
+		setRobotStatus("Idle", "idle.svg");
+	}
+});
+
+function setRobotStatus(t, robotSrc) {
+	statusText.innerHTML = t;
+	robot.src = robotSrc;
+}
+
+function messageActiveTab(message, callback = () => {}) {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, message, callback);
+	});
+}
